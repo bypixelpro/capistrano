69e6ec86d0c13d605a401d23da12890d6a775c57
bypixelpro <info@pixelpro.es>
2017-11-07 14:16:04 +0100

    Add files via upload

---
+++ config/unicorn.rb
@@ -0,0 +1,7 @@
+pid "/home/deploy/guestbook/current/tmp/pids/unicorn.pid"
+stderr_path "/home/deploy/guestbook/current/unicorn/unicorn.log"
+stdout_path "/home/deploy/guestbook/current/unicorn/unicorn.log"
+
+listen "/tmp/unicorn.guestbook.sock"
+worker_processes 2
+timeout 30
